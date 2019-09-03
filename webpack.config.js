954fd2f2b64d73bc39a6a125f6648a67e24880e2
Max Salles <herond12@gmail.com>
2019-09-02 23:21:54 -0300

    Configured StandardJS and editorconfig file

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,15 @@ module.exports = {
   module: {
     rules: [
       {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: {
+          loader: 'standard-loader'
+        }
+      },
+      {
         test: /\.js$/,
         exclude: /node_modules/,
         include: /src/,
