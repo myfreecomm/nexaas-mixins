9990b208020284f6e141537dbc830b38a7de5413
Max Salles <herond12@gmail.com>
2019-09-09 22:45:35 -0300

    Added dasherize helper

---
+++ src/helpers/dasherize.js
@@ -0,0 +1,25 @@
+const separatorRgx = /[A-Z]+|[\-|\_]+|[[a-z]|\d]+/g
+const isDash = str => ['-', '_'].includes(str.charAt(0))
+const isUpperCase = str => str === str.toUpperCase()
+const clearStartAndEndDashes = (str, i, arr) => !isDash(str) || (i !== 0 && i !== arr.length - 1)
+
+export default function dasherize (str) {
+  return str
+    .match(separatorRgx)
+    .filter(clearStartAndEndDashes)
+    .map((str, i, arr) => {
+      if (isDash(str)) return '-'
+
+      if (isUpperCase(str) && arr.length !== 1) {
+        const str1 = i !== 0 && !isDash(arr[i - 1]) ? '-' : ''
+        const str2 = str.substring(0, str.length - 1)
+        const str3 = str.length === 1 || isDash(arr[i + 1]) ? '' : '-'
+        const str4 = str.charAt(str.length - 1)
+
+        return (str1 + str2 + str3 + str4).toLowerCase()
+      }
+
+      return str.toLowerCase()
+    })
+    .join('')
+}
---
+++ test/helpers/dasherize.spec.js
@@ -0,0 +1,15 @@
+import dasherize from '../../src/helpers/dasherize'
+
+describe('dasherize', () => {
+  it('returns dash case "str"', () => {
+    expect(dasherize('aaabbb')).toBe('aaabbb')
+    expect(dasherize('aaaBbb')).toBe('aaa-bbb')
+    expect(dasherize('AaaBbb')).toBe('aaa-bbb')
+    expect(dasherize('aaABbb')).toBe('aa-a-bbb')
+    expect(dasherize('aAABbb')).toBe('a-aa-bbb')
+    expect(dasherize('AAABbb')).toBe('aaa-bbb')
+    expect(dasherize('AAABBb')).toBe('aaab-bb')
+    expect(dasherize('AAABBB')).toBe('aaabbb')
+    expect(dasherize('-_-Aa_ABB-b_-_')).toBe('aa-abb-b')
+  })
+})
