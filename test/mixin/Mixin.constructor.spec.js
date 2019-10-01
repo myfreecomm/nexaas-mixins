5c4e0d13c388de87e2c7ab0b6ed2335539bcbd33
Max Salles <herond12@gmail.com>
2019-10-01 01:13:41 -0300

    Added specs to Mixin constructor

---
+++ test/mixin/Mixin.constructor.spec.js
@@ -0,0 +1,10 @@
+import Mixin from '../../src/mixin/Mixin'
+
+describe('Mixin.constructor', () => {
+  it('assignes "_element" attribute with "element"', () => {
+    const element = {}
+    const instance = new Mixin(element)
+
+    expect(instance).toEqual({ _element: element })
+  })
+})
