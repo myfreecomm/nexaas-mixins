72dd8ec8d5044140634061c3ec25ec1475ccd2f5
Max Salles <herond12@gmail.com>
2019-10-01 01:16:56 -0300

    Added specs to Mixin reload

---
+++ test/mixin/Mixin.reload.spec.js
@@ -0,0 +1,34 @@
+jest.mock('../../src/mixin/attachMixinToElement')
+
+import Mixin from '../../src/mixin/Mixin'
+import attachMixinToElement from '../../src/mixin/attachMixinToElement'
+import store from '../../src/mixin/store'
+
+describe('Mixin.reload', () => {
+  beforeEach(() => attachMixinToElement.mockClear())
+  afterEach(() => document.body.innerHTML = '')
+
+  it('calls "instance.undo" and "attachMixinToElement"', () => {
+    document.body.innerHTML = '<input>'
+
+    const instance = { undo: jest.fn() }
+    const element = document.querySelector('input')
+    const customOptions = {}
+
+    store.set(element, Mixin.name, instance)
+
+    expect(Mixin.reload(element, customOptions)).toBe(Mixin)
+    expect(instance.undo).toBeCalled()
+    expect(attachMixinToElement).toBeCalledWith(element, customOptions, Mixin, Mixin.name, store)
+  })
+
+  it('doesn`t call "attachMixinToElement" if there isn`t already instance for this mixin associated with the "element"', () => {
+    document.body.innerHTML = '<input>'
+
+    const element = document.querySelector('input')
+    const customOptions = {}
+
+    expect(Mixin.reload(element, customOptions)).toBe(Mixin)
+    expect(attachMixinToElement).not.toBeCalled()
+  })
+})
