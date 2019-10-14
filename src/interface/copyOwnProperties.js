d01eb3b06d840d6aa648e2661c5c00912b075c93
Max Salles <herond12@gmail.com>
2019-10-13 23:33:08 -0300

    Added copyOwnProperties function

---
+++ src/interface/copyOwnProperties.js
@@ -0,0 +1,9 @@
+export default function copyOwnProperties (target, ...sources) {
+  for (const source of sources) {
+    for (const prop in source) {
+      const descriptor = Object.getOwnPropertyDescriptor(source, prop)
+
+      if (descriptor) Object.defineProperty(target, descriptor)
+    }
+  }
+}
