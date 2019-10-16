69b2adba2107b561f8756748a6145dddefb54109
Max Salles <herond12@gmail.com>
2019-10-16 00:53:15 -0300

    Added spec to interfacePropName

---
+++ test/interface/interfacePropName.spec.js
@@ -0,0 +1,7 @@
+import interfacePropName from '../../src/interface/interfacePropName'
+
+describe('interfacePropName', () => {
+  it('is "__implements__"', () => {
+    expect(interfacePropName).toBe('__implements__')
+  })
+})
