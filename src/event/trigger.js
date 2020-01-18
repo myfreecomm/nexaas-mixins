c3f91287da6ffc15730989fe200a040bf38e528a
Max Salles <herond12@gmail.com>
2020-01-18 12:26:41 -0300

    Added trigger to Event interface

---
+++ src/event/trigger.js
@@ -0,0 +1,26 @@
+import eventPropName from './eventPropName'
+
+export default function trigger (type, data, thisObject) {
+  if (!(eventPropName in this)) return this
+
+  let callbacks = []
+
+  for (const registered in this[eventPropName]) {
+    const hasEqualLenght = registered.length === type.length
+    const areTheSame = registered === type
+    const isContext = (
+      registered.substring(0, type.length) === type &&
+      registered.charAt(type.length) === '.'
+    )
+
+    if (hasEqualLenght && areTheSame || isContext) {
+      callbacks = callbacks.concat(this[eventPropName][registered])
+    }
+  }
+
+  for (const callback of callbacks) {
+    callback.call(thisObject === undefined ? this : thisObject, data)
+  }
+
+  return this
+}
