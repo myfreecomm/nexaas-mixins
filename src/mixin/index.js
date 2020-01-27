0025b05cb026394475a13dc159f6b9f3666444fb
Max Salles <herond12@gmail.com>
2020-01-27 01:07:57 -0300

    Fixed mixin mount

--- src/mixin/index.js
+++ src/mixin/index.js
@@ -1,3 +1,5 @@
 import Mixin from './Mixin'
+import mount from './mount'
 
 export default Mixin
+export { mount }
--- src/mixin/mount.js
+++ src/mixin/mount.js
@@ -1,10 +1,12 @@
 export default async function mount (root, { mixins }) {
   const DOMTraversal = document.createNodeIterator(root, NodeFilter.SHOW_ELEMENT)
-  let element
+  let element = DOMTraversal.nextNode()
 
-  while (element = DOMTraversal.nextNode()) {
+  while (element) {
     for (const Mixin of mixins) {
       if (Mixin.isApplicable(element)) Mixin.apply(element)
     }
+
+    element = DOMTraversal.nextNode()
   }
 }
