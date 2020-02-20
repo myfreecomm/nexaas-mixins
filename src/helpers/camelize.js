262022dba97ca1552ce0f1375e84434614852e59
Max Salles <herond12@gmail.com>
2020-02-19 23:40:19 -0300

    Added startsWithUpperCase option to camelize and refactored specs

--- src/helpers/camelize.js
+++ src/helpers/camelize.js
@@ -1,10 +1,10 @@
-export default function camelize (str) {
+export default function camelize (str, startsWithUpperCase = false) {
   const splited = str.split(/[\s|_|-]+/g)
 
   return splited.reduce(
     (acc, word, index) => (
       acc + (
-        index
+        index || startsWithUpperCase
           ? word.charAt(0).toUpperCase() + word.slice(1)
           : word.charAt(0).toLowerCase() + word.slice(1)
       )
--- test/helpers/camelize.spec.js
+++ test/helpers/camelize.spec.js
@@ -1,9 +1,17 @@
 import camelize from '../../src/helpers/camelize'
 
 describe('camelize', () => {
-  it('shuld return camel case "str"', () => {
-    const str = 'Lorem ipsum-dolor_sit--amet'
+  const str = 'Lorem ipsum-dolor_sit--amet'
 
+  it('returns camel case "str"', () => {
     expect(camelize(str)).toBe('loremIpsumDolorSitAmet')
+  })
+
+  describe('when "startsWithUpperCase" is "true"', () => {
+    it('returns camel case "str" with first capital letter', () => {
+      const startsWithUpperCase = true
+
+      expect(camelize(str, startsWithUpperCase)).toBe('LoremIpsumDolorSitAmet')
+    })
   })
 })
