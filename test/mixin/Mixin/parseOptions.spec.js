be323e3845af29c14f407094bcda5438639d03b3
Max Salles <herond12@gmail.com>
2020-02-22 00:38:14 -0300

    Refactored specs to Mixin.parseOptions

--- test/mixin/Mixin/parseOptions.spec.js
+++ test/mixin/Mixin/parseOptions.spec.js
@@ -1,67 +1,37 @@
 import Mixin from '../../../src/mixin/Mixin'
 
 describe('Mixin.parseOptions', () => {
-  afterEach(() => document.body.innerHTML = '')
+  it('returns JSON parse when "str" is a valid JSON', () => {
+    const str = '{ "data": 20, "pattern": "string", "arr": [1, 2, 3] }'
 
-  it('returns parsed JSON when "element" attribute value is a valid JSON', () => {
-    document.body.innerHTML = `<input mx-mixin="${
-      '{&quot;data&quot;: 20, &quot;patter&quot;: &quot;string&quot;, &quot;arr&quot;: [1, 2, 3]}'
-    }">`
-
-    const element = document.querySelector('input')
-
-    expect(Mixin.parseOptions(element)).toEqual(
-      { data: 20, patter: 'string', arr: [1, 2, 3] }
-    )
+    expect(Mixin.parseOptions(str)).toEqual({ data: 20, pattern: 'string', arr: [1, 2, 3] })
   })
 
-  it('returns a "Number" when "element" attribute value is a valid number', () => {
-    document.body.innerHTML = '<input mx-mixin="22.2">'
+  it('returns a "Number" when "str" is a valid number', () => {
+    const num = '22.2'
 
-    const element = document.querySelector('input')
-
-    expect(Mixin.parseOptions(element)).toBe(22.2)
+    expect(Mixin.parseOptions(num)).toBe(22.2)
   })
 
-  it('returns "true" when "element" attribute value is "true"', () => {
-    document.body.innerHTML = '<input mx-mixin="true">'
+  it('returns a "Boolean" when "str" is a valid boolean', () => {
+    const strTrue  = 'true'
+    const strFalse = 'false'
 
-    const element = document.querySelector('input')
-
-    expect(Mixin.parseOptions(element)).toBe(true)
+    expect(Mixin.parseOptions(strTrue)).toBe(true)
+    expect(Mixin.parseOptions(strFalse)).toBe(false)
   })
 
-  it('returns "false" when "element" attribute value is "false"', () => {
-    document.body.innerHTML = '<input mx-mixin="false">'
+  it('returns itself "str" when params is not a valid JSON', () => {
+    const str  = 'foo'
+    const json = '{ "data: 20, pattern: "string", "arr": [1, 2, 3] }'
 
-    const element = document.querySelector('input')
-
-    expect(Mixin.parseOptions(element)).toBeFalsy()
+    expect(Mixin.parseOptions(str)).toEqual(str)
+    expect(Mixin.parseOptions(json)).toEqual(json)
   })
 
-  it('returns "element" attribute value itself if isn`t a valid JSON', () => {
-    document.body.innerHTML = `<input mx-mixin="${
-      '{ &quot;data: 20, pattern: &quot;string&quot;, &quot;arr&quot;: [1, 2, 3] }'
-    }">`
-
-    const element = document.querySelector('input')
-
-    expect(Mixin.parseOptions(element)).toBe('{ "data: 20, pattern: "string", "arr": [1, 2, 3] }')
-  })
-
-  it('returns "undefined" when "element" attribute value is an empty string', () => {
-    document.body.innerHTML = '<input mx-mixin="">'
-
-    const element = document.querySelector('input')
-
-    expect(Mixin.parseOptions(element)).toBeUndefined()
-  })
-
-  it('returns "undefined" when "element" attribute there isn`t value', () => {
-    document.body.innerHTML = '<input mx-mixin>'
-
-    const element = document.querySelector('input')
-
-    expect(Mixin.parseOptions(element)).toBeUndefined()
+  it('returns "undefined" when params is an empty string, "undefined" or "null"', () =>{
+    expect(Mixin.parseOptions('')).toBeUndefined()
+    expect(Mixin.parseOptions()).toBeUndefined()
+    expect(Mixin.parseOptions(null)).toBeUndefined()
   })
 })
