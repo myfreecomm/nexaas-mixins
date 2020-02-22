5a60679a863f334eaa9574efd088443a55217f1d
Max Salles <herond12@gmail.com>
2020-02-22 00:36:12 -0300

    Embedded mount to Mixin

--- src/mixin/Mixin.js
+++ src/mixin/Mixin.js
@@ -1,16 +1,14 @@
-import { parseDataString } from '../helpers'
+import { camelize } from '../helpers'
+import Event from '../event'
 
 import getMixinAttributeName from './getMixinAttributeName'
 import attachMixinToElement from './attachMixinToElement'
 import store from './store'
+import getOrSetElementId from './getOrSetElementId'
 
 export default class Mixin {
   constructor (element) {
     this._element = element
-  }
-
-  get element () {
-    return this._element
   }
 
   execute () {
@@ -21,39 +19,47 @@ export default class Mixin {
     throw new Error('Not implemented')
   }
 
+  get element () {
+    return this._element
+  }
+
   static get name () {
     return 'Mixin'
   }
 
-  static isApplicable (element) {
-    const attr = getMixinAttributeName(this.name)
+  static getUnparsedOptions (element) {
+    const attributeName = getMixinAttributeName(this.name)
 
-    return element.getAttribute(attr) !== null
+    return element.getAttribute(attributeName)
   }
 
-  static parseOptions (element) {
-    const attr = getMixinAttributeName(this.name)
-    const stringOptions = element.getAttribute(attr)
+  static parseOptions (str) {
+    if (['', undefined, null].includes(str)) return undefined
 
-    return parseDataString(stringOptions)
+    try {
+      return JSON.parse(str)
+    } catch (_) {
+      return str
+    }
   }
 
-  static apply (element, customOptions) {
+  static apply (element, options) {
     const instance = this.get(element)
 
     if (instance) return this
-    attachMixinToElement(element, customOptions, this, this.name, store)
+
+    attachMixinToElement(element, options, this, this.name, store)
 
     return this
   }
 
-  static reload (element, customOptions) {
+  static reload (element, options) {
     const instance = this.get(element)
 
     if (!instance) return this
 
     instance.undo()
-    attachMixinToElement(element, customOptions, this, this.name, store)
+    attachMixinToElement(element, options, this, this.name, store)
 
     return this
   }
@@ -70,4 +76,33 @@ export default class Mixin {
   static get (element) {
     return store.get(element, this.name)
   }
+
+  static mount (root, { mixins }) {
+    const children = root.children
+    const id = getOrSetElementId(root)
+
+    this.trigger(`mx.mount.start.block.${id}`, root)
+    this.trigger(`mx.mount.start.element.${id}`, root)
+
+    for (const { name, value } of root.attributes) {
+      if (!(/^mx-/).test(name)) continue
+
+      const mixinName = camelize(name.substring(3), true)
+      const Mixin = mixins.find(M => M.name === mixinName)
+
+      if (Mixin) Mixin.apply(root, Mixin.parseOptions(value))
+    }
+
+    this.trigger(`mx.mount.end.element.${id}`, root)
+
+    if (children.length) {
+      const { forEach } = Array.prototype
+
+      forEach.call(children, child => this.mount(child, { mixins }))
+    }
+
+    this.trigger(`mx.mount.end.block.${id}`, root)
+  }
 }
+
+Event.apply(Mixin)
--- src/mixin/attachMixinToElement.js
+++ src/mixin/attachMixinToElement.js
@@ -1,5 +1,5 @@
 export default function attachMixinToElement (element, customOptions, Mixin, key, store) {
-  const options = customOptions || Mixin.parseOptions(element)
+  const options = customOptions || Mixin.parseOptions(Mixin.getUnparsedOptions(element))
   const instance = new Mixin(element, options)
 
   store.set(element, key, instance)
